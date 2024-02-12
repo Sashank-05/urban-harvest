d5a02f8c27c38b75ca5cf841f4bb2a2d4d0bd8c7
Sashank-05 <sashankvanka@outlook.com>
2024-02-12 15:20:36 +0530

    added font

--- lib/landing/landing.dart
+++ lib/landing/landing.dart
@@ -1,28 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:urban_harvest/constant_colors.dart';
 
 class LandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Landing Page'),
+        toolbarOpacity: 0,
+        title: const Text("Urban Harvest", style: TextStyle(
+          fontFamily: 'Montserrat',
+        )),
       ),
-      body: Center(
+      body: const Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Welcome to the Landing Page!',
-              style: TextStyle(fontSize: 24.0),
-            ),
-            SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: () {
-                // Add functionality for signing out here
-              },
-              child: Text('Sign Out'),
-            ),
-          ],
+          children: [],
         ),
       ),
     );

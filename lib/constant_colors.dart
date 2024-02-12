ee2f66a5e5ecf4bd46fba680d8e913fd6cb5748b
Sashank-05 <sashankvanka@outlook.com>
2024-02-12 15:09:58 +0530

    added color constants

---
+++ lib/constant_colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color.fromRGBO(216, 243, 220,1);
+  static const Color secondaryColor = Color.fromRGBO(183, 228, 199,1);
+  static const Color secondaryColor2 = Color.fromRGBO(149, 213, 178,1);
+  static const Color secondaryColor3 = Color.fromRGBO(116, 198, 157,1);
+  static const Color tertiaryColor = Color.fromRGBO(82, 183, 136,1);
+  static const Color tertiaryColor2 = Color.fromRGBO(64, 145, 108,1);
+  static const Color backgroundColor = Color.fromRGBO(8, 28, 21, 1);
+  static const Color backgroundColor2 = Color.fromRGBO(27, 67, 50, 1);
+  static const Color backgroundColor3 = Color.fromRGBO(45, 106, 79,1);
+  static const Color textColorLight = Color.fromRGBO(0, 0, 0, 1);
+  static const Color textColorDark = Color.fromRGBO(167, 255, 230, 1.0);
+
+}

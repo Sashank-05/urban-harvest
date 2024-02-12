981e6ea071186b190db343f61806181a1fc18938
Sashank-05 <sashankvanka@outlook.com>
2024-02-12 15:44:58 +0530

    testing changes

--- lib/landing/landing.dart
+++ lib/landing/landing.dart
@@ -2,16 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:urban_harvest/constant_colors.dart';
 
 class LandingPage extends StatelessWidget {
+  const LandingPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.backgroundColor,
       appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: AppColors.backgroundColor2,
         toolbarOpacity: 0,
         title: const Text("Urban Harvest", style: TextStyle(
           fontFamily: 'Montserrat',
+          fontWeight:FontWeight.bold ,
+          color: AppColors.primaryColor,
+
         )),
       ),
       body: const Center(
+
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [],

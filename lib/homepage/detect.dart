bd26edf7881c281f09b8ca52da0315e40161b8f8
MrLuffyMom <137860111+pranav244872@users.noreply.github.com>
2024-02-25 14:45:25 +0530

    improved detection page

--- lib/homepage/detect.dart
+++ lib/homepage/detect.dart
@@ -8,6 +8,154 @@ import 'package:image_picker/image_picker.dart';
 
 import '../constant_colors.dart';
 
+class InferenceResultPage extends StatelessWidget {
+  final File imageFile;
+  final String inferenceResult;
+
+  const InferenceResultPage({
+    required this.imageFile,
+    required this.inferenceResult,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.backgroundColor,
+      appBar: AppBar(
+        backgroundColor: AppColors.backgroundColor2,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          color: const Color(0xFFD8F3DC),
+          iconSize: 25,
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: const Text(
+          'Inference Result',
+          style: TextStyle(
+            fontFamily: 'Montserrat',
+            color: AppColors.primaryColor,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Expanded(
+            child: Container(
+                width: double.infinity,
+                padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                margin: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: AppColors.backgroundColor3),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Container(
+                        margin: const EdgeInsets.only(bottom: 20),
+                        child: const Text(
+                          'Picture used for inference',
+                          style: TextStyle(
+                              fontFamily: 'Montserrat',
+                              color: AppColors.primaryColor,
+                              fontSize: 20),
+                        )),
+                    Flexible(
+                      child: Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(30),
+                              color: AppColors.tertiaryColor2),
+                          padding: const EdgeInsets.all(20),
+                          child: Image.file(imageFile, width: 300, height: 300,)),
+                    ),
+                  ],
+                )),
+          ),
+          Container(
+            width: double.infinity,
+            padding: const EdgeInsets.all(20),
+            margin: const EdgeInsets.only(bottom: 20, right: 20, left: 20),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: AppColors.backgroundColor3),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(bottom: 10.0),
+                  child: Text(
+                    'Detected plant condition',
+                    style: TextStyle(
+                      color: AppColors.primaryColor,
+                      fontFamily: 'Montserrat',
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(color: AppColors.tertiaryColor2, borderRadius: BorderRadius.circular(30)),
+                  padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                  child: Text(
+                    inferenceResult,
+                    style: const TextStyle(
+                      color: AppColors.primaryColor,
+                      fontFamily: 'Montserrat',
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            padding: const EdgeInsets.all(20),
+            margin: const EdgeInsets.only(bottom: 20, right: 20, left: 20),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: AppColors.backgroundColor3),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(bottom: 10.0),
+                  child: Text(
+                    'Solution',
+                    style: TextStyle(
+                      color: AppColors.primaryColor,
+                      fontFamily: 'Montserrat',
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(color: AppColors.tertiaryColor2, borderRadius: BorderRadius.circular(30)),
+                  padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                  child: const Text(
+                    'For a quick, organic solution to common maize rust, prioritize planting resistant varieties. If rust appears, act fast! Mix 1 tablespoon of potassium bicarbonate per gallon of water and spray undersides of leaves every 7-10 days. Boost plant health with diluted compost tea around the base. Remember, early action is key!',
+                    style: TextStyle(
+                      color: AppColors.primaryColor,
+                      fontFamily: 'Montserrat',
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 class InferencePage extends StatefulWidget {
   @override
   _InferencePageState createState() => _InferencePageState();
@@ -43,7 +191,7 @@ class _InferencePageState extends State<InferencePage> {
     }
   }
 
-  Future<void> _runInference(File imageFile) async {
+  Future<void> _runInference(BuildContext context, File imageFile) async {
     try {
       final inputDetails = _interpreter.getInputTensors();
       final outputDetails = _interpreter.getOutputTensors();
@@ -80,13 +228,22 @@ class _InferencePageState extends State<InferencePage> {
         print('${_classes[i]}: ${outputData[i]}');
       }
       final predictedClassIdx =
-      outputData.indexOf(outputData.reduce((a, b) => a > b ? a : b));
+          outputData.indexOf(outputData.reduce((a, b) => a > b ? a : b));
       final predictedClass = _classes[predictedClassIdx];
 
       // Update UI or state
       setState(() {
-        _result = 'Predicted plant condition: $predictedClass';
+        _result = '$predictedClass';
       });
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => InferenceResultPage(
+            imageFile: imageFile,
+            inferenceResult: _result,
+          ),
+        ),
+      );
     } catch (e) {
       // Handle any errors
       setState(() {
@@ -95,12 +252,11 @@ class _InferencePageState extends State<InferencePage> {
     }
   }
 
-
-  Future<void> _pickImage(ImageSource source) async {
+  Future<void> _pickImage(BuildContext context, ImageSource source) async {
     final picker = ImagePicker();
     final pickedFile = await picker.pickImage(source: source);
     if (pickedFile != null) {
-      await _runInference(File(pickedFile.path));
+      await _runInference(context, File(pickedFile.path));
     }
   }
 
@@ -108,45 +264,76 @@ class _InferencePageState extends State<InferencePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.backgroundColor,
-      appBar: AppBar( backgroundColor: AppColors.backgroundColor2,
+      appBar: AppBar(
+        backgroundColor: AppColors.backgroundColor2,
         leading: IconButton(
-          icon: Padding(
+          icon: const Padding(
               padding: EdgeInsets.only(top: 15), child: Icon(Icons.arrow_back)),
-          color: Color(0xFFD8F3DC),
+          color: const Color(0xFFD8F3DC),
           iconSize: 25,
           onPressed: () {
             Navigator.pop(context);
           },
         ),
-        title: Text('Detect Dieases ', style: TextStyle(
-        fontFamily: 'Montserrat',
-        color: AppColors.primaryColor),
-       ),
+        title: const Text(
+          'Detect Disease',
+          style: TextStyle(
+              fontFamily: 'Montserrat', color: AppColors.primaryColor),
+        ),
       ),
-    body: Center(
+      body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Image.asset('assets/img/landing/plantdisease.png'),
             const Padding(
-              padding: EdgeInsets.only(bottom: 40.0,left: 20,right: 20),
-              child: Text(
-                'Know what is Ailing your Plant with a Click!',
-                style: TextStyle(
-                    color: AppColors.textColorDark,
-                    fontFamily: 'Montserrat',
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold),
+              padding: EdgeInsets.only(bottom: 40.0, left: 20, right: 20),
+              child: Center(
+                child: Text(
+                  'Know what is Ailing your Plant with a Click!',
+                  style: TextStyle(
+                      color: AppColors.primaryColor,
+                      fontFamily: 'Montserrat',
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+                ),
               ),
             ),
-            SizedBox(height: 40),
-            ElevatedButton.icon(onPressed: () => _pickImage(ImageSource.camera), icon:Image.asset('assets/img/camera.png',width: 40,height: 40,), label: Text('Take a Picture',style: TextStyle(color: AppColors.textColorDark,fontFamily: 'Montserrat'),),style: ElevatedButton.styleFrom(backgroundColor: AppColors.tertiaryColor2,fixedSize: const Size(350, 50)),)
-    ,
-            SizedBox(height: 20),
-
-            ElevatedButton.icon(onPressed: () => _pickImage(ImageSource.gallery), icon:Image.asset('assets/img/gallery.png',width: 40,height: 40,), label: Text('Pick an Image from Gallery',style: TextStyle(color: AppColors.textColorDark,fontFamily: 'Montserrat'),),style: ElevatedButton.styleFrom(backgroundColor: AppColors.tertiaryColor2,fixedSize: const Size(350, 50)),)
-            ,
-            SizedBox(height: 20),
-            Text(_result,style: TextStyle(color: AppColors.textColorDark),textAlign: TextAlign.center,),
+            const SizedBox(height: 40),
+            ElevatedButton.icon(
+              onPressed: () => _pickImage(context, ImageSource.camera),
+              icon: Image.asset(
+                'assets/img/camera.png',
+                width: 30,
+                height: 30,
+              ),
+              label: const Text(
+                'Take a Picture',
+                style: TextStyle(
+                    color: AppColors.textColorDark, fontFamily: 'Montserrat'),
+              ),
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: AppColors.tertiaryColor2,
+                  fixedSize: const Size(350, 50)),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton.icon(
+              onPressed: () => _pickImage(context, ImageSource.gallery),
+              icon: Image.asset(
+                'assets/img/gallery.png',
+                height: 30,
+                width: 30,
+              ),
+              label: const Text(
+                'Pick an Image from Gallery',
+                style: TextStyle(
+                    color: AppColors.textColorDark, fontFamily: 'Montserrat'),
+              ),
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: AppColors.tertiaryColor2,
+                  fixedSize: const Size(350, 50)),
+            ),
+            const SizedBox(height: 20),
           ],
         ),
       ),
